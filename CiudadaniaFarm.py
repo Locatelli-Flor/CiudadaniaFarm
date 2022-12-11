a814a8818bd4ca012fb0238f4f601e84688e9ce4
Locatelli-Flor <110829822+Locatelli-Flor@users.noreply.github.com>
2022-12-11 17:57:26 -0300

    Add files via upload

---
+++ CiudadaniaFarm.py
@@ -0,0 +1,52 @@
+import requests
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Setup de navegador
+
+browserDriver = webdriver.Chrome("C:/Users/Flori/Desktop/Cucu/Cosas/msedgedriver.exe")
+browserDriver.get("https://prenotami.esteri.it/Home?ReturnUrl=%2fServices")
+
+# Iniciar sesión
+
+email = "locatelliflor@gmail.com"
+contraseña = "Flm12345"
+
+input_email = '//*[@id="login-email"]'
+input_contraseña = '//*[@id="login-password"]'
+
+browserDriver.find_element(By.XPATH, input_email).send_keys(email)
+browserDriver.find_element(By.XPATH, input_contraseña).send_keys(contraseña)
+
+iniciarSesion = browserDriver.find_element(By.XPATH , '//*[@id="login-form"]/button')
+iniciarSesion.click()
+
+time.sleep(2)
+
+# Hacer click en el botón de reserva
+
+boton = browserDriver.find_element(By.XPATH,'//*[@id="dataTableServices"]/tbody/tr[3]/td[4]/a/button')
+boton.click()
+
+# Función que retorna true si se accedió al formulario, y false si no.
+
+def validacion():
+    try:
+        browserDriver.find_element(By.ID, "jconfirm-box96825")
+    except:
+        return False
+
+    return True
+
+# Si se pudo acceder al formulario, se ingresan los datos
+
+if(validacion()):
+    print("Entré c:")
+
+else:
+    print("No entré :c")
+
+
